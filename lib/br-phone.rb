c0142b9f3fc4d2c9102157036842e1e847b14c90
Fabian <fabian.pow@gmail.com>
2016-11-18 16:56:03 -0300

    Generate randon phone number

--- lib/br-phone.rb
+++ lib/br-phone.rb
@@ -1,5 +1,5 @@
 class Br_phone
-  def self.generate
-    true
+  def self.generate_cell_phone
+    "+5565#{rand(10**5)}#{rand(10**4)}"
   end
 end
--- spec/br_phone_spec.rb
+++ spec/br_phone_spec.rb
@@ -1,4 +1,18 @@
 require 'spec_helper'
 describe Br_phone do
-  pending "write it"
+  describe "#generate_cell_phone" do
+  	it "return a cell phone number" do
+  		cell_phone = Br_phone.generate_cell_phone
+
+  		expect(cell_phone[0,3]).to eq('+55')
+  		expect(cell_phone[3,2]).to eq('65')
+  	end
+
+  	it "return a randon cell phone number" do
+  		cell_phone_01 = Br_phone.generate_cell_phone
+  		cell_phone_02 = Br_phone.generate_cell_phone
+
+  		expect(cell_phone_01).to_not eq(cell_phone_02)
+  	end
+  end
 end
