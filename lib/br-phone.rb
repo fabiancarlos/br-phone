60cd1aad412a032cf52373197cdbdcf114e9edf5
Fabian <fabian.pow@gmail.com>
2016-11-20 18:29:25 -0300

    Refactoring generate and validate cell phones

--- lib/br-phone.rb
+++ lib/br-phone.rb
@@ -1,17 +1,33 @@
 class Br_phone
+  def initialize(phone)
+    @phone = phone
+  end
+
   def self.generate_cell_phone
-    "+5565#{rand(10**5)}#{rand(10**4)}"
+    "+55#{'%07d' % rand(10**7)}#{'%04d' % rand(10**4)}"
   end
 
-  def self.phone_is_valid?(phone)
-  	result = phone =~ /\A[+]?[0-9]*\.?[0-9]+\Z/
-  	return false if result.nil? # true is 0 (zero)
+  def return_valid
+    return nil if @phone.nil?
+    result = (is_valid? @phone) ? @phone.gsub(/\D+/, "") : '-'
 
-  	result = phone.gsub(/\D+/, "")
-  	return result.size == 13 || result.size == 12
+    case (result.size || 0)
+      when 13
+        '+' << result
+      when 12
+        '+55' << result
+      else
+        nil
+    end
   end
 
-  def self.return_valid_phone(phone)
-  	result = phone.gsub(/\D+/, "").strip
+  private
+
+  def is_valid?(phone)
+    result = phone =~ /\A[+]?[0-9]*\.?[0-9]+\Z/
+    return false if result.nil?
+
+    result = phone.gsub(/\D+/, "")
+    return result.size == 13 || result.size == 12
   end
 end
--- spec/br_phone_spec.rb
+++ spec/br_phone_spec.rb
@@ -2,36 +2,39 @@ require 'spec_helper'
 describe Br_phone do
 
   VALID_PHONES = ['+5565425492527', '5565425492527', '065425492527', '0146533421647']
-  INVALID_PHONES = ['+55654254925', '65425492527', '33421647', 'AAASBB', 'exit()', '', '556542549252!', '+55 65 42549252', '+55 65 842549252']
+  INVALID_PHONES = ['+55654254925', '-5565425492527', '65425492527', '33421647', 'AAASBB', 'exit()', '', '556542549252!', '+55 65 42549252', '+55 65 842549252']
+  
+  before(:each) do
+    @cell_phone_1 = Br_phone.generate_cell_phone
+    @cell_phone_2 = Br_phone.generate_cell_phone
+  end
 
-  describe "#generate_cell_phone" do
-  	it "return a cell phone number" do
-  		cell_phone = Br_phone.generate_cell_phone
+  20.times.collect do
+    describe "#generate_cell_phone" do
+      it "return a valid cell phone number" do
+        expect(Br_phone.new(@cell_phone_1).return_valid.size).to eq(14)
+        expect(Br_phone.new(@cell_phone_1).return_valid).to_not eq('')
+        expect(Br_phone.new(@cell_phone_1).return_valid[0,3]).to eq('+55')
+      end
 
-  		expect(cell_phone[0,3]).to eq('+55')
-  		expect(cell_phone[3,2]).to eq('65')
-  	end
-
-  	it "return a randon cell phone number" do
-  		cell_phone_01 = Br_phone.generate_cell_phone
-  		cell_phone_02 = Br_phone.generate_cell_phone
-
-  		expect(cell_phone_01).to_not eq(cell_phone_02)
-  	end
+      it "return a randon cell phone number" do
+        expect(@cell_phone_1).to_not eq(@cell_phone_2)
+      end
+    end
   end
 
   VALID_PHONES.each do |phone|
     describe "#phone_is_valid? (#{phone})" do
-      it "return true if phone is valid" do
-        expect(Br_phone.phone_is_valid? phone).to eq(true)
+      it "return phone if is valid" do
+        expect(Br_phone.new(phone).return_valid).to be_truthy
       end
     end
   end
 
   INVALID_PHONES.each do |phone|
     describe "#phone_is_valid? (#{phone})" do
-      it "return false if phone is invalid" do
-        expect(Br_phone.phone_is_valid? phone).to eq(false)
+      it "return nil if is invalid" do
+        expect(Br_phone.new(phone).return_valid).to be_falsey
       end
     end
   end
